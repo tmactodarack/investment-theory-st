2406fa0acf3ec79e3daa6a577c3c11f6d330f3ea
tmactodarack <158004222+tmactodarack@users.noreply.github.com>
2024-02-23 18:39:49 -0600

    Add files via upload

--- pages/wage.py
+++ pages/wage.py
@@ -46,7 +46,9 @@ for i in tickers.keys():
     filt = (df['value'] != '.') # some data from fred is in weird . string
     single = df[filt]['value'].apply(lambda x: float(x)).to_frame(tickers[i]) # excluding . and turn into float
     combine = pd.concat([combine, single], axis=1)
-    combine.index.name = None
+
+combine.index.name = None
+combine = combine.sort_index()
 ##################################################################################################
 
 
@@ -56,14 +58,17 @@ longTerm_fig = go.Figure()
 longTerm_fig.add_trace(go.Line(x=longTerm.index, y=longTerm['Employment Cost Index'], name='ECI YoY'))
 longTerm_fig.add_trace(go.Line(x=longTerm.index, y=longTerm['Average Hourly Earnings'], name='AHE YoY'))
 longTerm_fig.add_trace(go.Line(x=longTerm.index, y=longTerm['Core PCE'], name='Core PCE YoY'))
-longTerm_fig.update_layout(template='ggplot2', 
+longTerm_fig.update_layout(template='seaborn', 
                     showlegend=True,
-                    title=dict(text='Long Term Relationship', x=0.03, y=0.9, font=dict(size=30)),
+                    title=dict(text='Long Term Relationship', y=0.9, font=dict(size=30)),
                     # title_x=0.1,
-                    legend = dict(orientation='h', y=1.0, x=1, xanchor='right', yanchor='bottom'),
+                    legend = dict(orientation='h'),
                     margin=dict(b=50,l=70,r=70,t=70),
-                    yaxis=dict(tickformat='.1%')
+                    xaxis=dict(fixedrange=True),
+                    yaxis=dict(tickformat='.1%', fixedrange=True)
                     )
+# longTerm_fig.update_yaxes(automargin=True)
+# longTerm_fig.update_xaxes(automargin=True)
 st.plotly_chart(longTerm_fig, use_container_width=True)
 st.markdown('''
             The core inflation is not highly tied to wage inflation, or to put it more generally, 
@@ -89,20 +94,18 @@ fig.add_trace(go.Scatter(x=[eci_qoq.index[-1]],
                          mode='markers',
                          marker_symbol='star',
                          marker_size=11 , 
-                         name='Update: ' + f"{updates['Employment Cost Index']:%m/%d/%y}"))
+                         name='Release: ' + f"{updates['Employment Cost Index']:%m/%d/%y}"))
 # fig.add_annotation(x=test.index[-1], y=test[-1], text='Latest Release')
-fig.update_layout(template='ggplot2', 
+fig.update_layout(template='seaborn', 
                   showlegend=True,
-                  title=dict(text=title, x=0.03, y=0.9, font=dict(size=30)),
+                  title=dict(text=title, y=0.9, font=dict(size=30)),
                 #   title_x=0.1,
-                #   legend = dict(orientation='h', y=1.02, x=1, xanchor='right'),
+                  legend = dict(orientation='h'),
                   margin=dict(b=50,l=70,r=70,t=70),
                   xaxis=dict(tickvals = eci_qoq.index, ticktext = eci_qoq.index.to_period('q').to_series().astype(str), fixedrange=True), 
                   yaxis=dict(tickformat='.1%', fixedrange=True)
                 )
-
-with st.container():
-    st.plotly_chart(fig, use_container_width=True)
+st.plotly_chart(fig, use_container_width=True)
 
 with st.expander(' :bulb: Tips'):
     st.markdown('''
@@ -119,19 +122,24 @@ eci_yoy = combine['Employment Cost Index'].dropna().pct_change(4).loc[start:end]
 
 eci_yoy_fig = go.Figure()
 eci_yoy_fig.add_trace(go.Line(x=eci_yoy.index, y=eci_yoy, name='ECI YoY'))
-eci_yoy_fig.add_trace(go.Scatter(x=[eci_yoy.index[-1]], 
-                         y=[eci_yoy[-1]], 
-                         mode='markers',
-                         marker_symbol='star',
-                         marker_size=11, 
-                         name='Update: '+f"{updates['Employment Cost Index']:%m/%d/%y}"))
-eci_yoy_fig.update_layout(template='ggplot2', 
+eci_yoy_fig.add_trace(go.Scatter(x=[eci_yoy.index[-1]], y=[eci_yoy[-1]], 
+                                 mode='markers', marker_symbol='star', marker_size=11, name='Release: '+f"{updates['Employment Cost Index']:%m/%d/%y}"))
+eci_yoy_fig.update_layout(template='seaborn', 
                     showlegend=True,
                     title=dict(text='Employment Cost Index YoY', x=0.03, y=0.9, font=dict(size=30)),
                     # title_x=0.1,
                     legend = dict(orientation='h', y=1.0, x=1, xanchor='right', yanchor='bottom'),
                     margin=dict(b=50,l=70,r=70,t=70),
-                    xaxis=dict(tickvals = eci_yoy.index, ticktext = eci_yoy.index.to_period('q').to_series().astype(str)), 
-                    yaxis=dict(tickformat='.1%')
+                    xaxis=dict(tickvals = eci_yoy.index, ticktext = eci_yoy.index.to_period('q').to_series().astype(str), fixedrange=True), 
+                    yaxis=dict(tickformat='.1%', fixedrange=True)
                     )
 st.plotly_chart(eci_yoy_fig, use_container_width=True)
+st.markdown('***')
+
+############### AHE mom ###########################
+start = '2022'
+ahe_mom = combine['Average Hourly Earnings'].pct_change().dropna().loc['2023':'2024']
+ahe_mom_fig = go.Figure()
+ahe_mom_fig.add_trace( go.Scatter(x=ahe_mom.index, y=ahe_mom, mode='lines+markers', name='Average Hourly Earnings MoM') )
+ahe_mom_fig.add_trace( go.Scatter(x=[ahe_mom.index[-1]], y=[ahe_mom[-1]], mode='markers', name=f"Release: {updates['Average Hourly Earnings']:%m/%d/%y}") )
+st.plotly_chart(ahe_mom_fig, use_container_width=True)
